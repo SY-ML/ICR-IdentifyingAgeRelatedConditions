a1616fce9483d60145b17735e62f24f17d9360b9
SY-ML <ai.engr.sy@gmail.com>
2023-06-13 23:49:28 +0900

    061323

--- main.py
+++ main.py
@@ -12,10 +12,47 @@ profile_test = ProfileReport(df_test, title="Test Set Profiling Report")
 profile_greek = ProfileReport(df_geek, title="Greeks Set Profiling Report")
 
 # Save the reports as HTML files
-path_report = './pandas_profiling_reports'
-os.mkdir(path_report) if os.path.exists('./pandas_profiling_reports') else 0
-profile_train.to_file("train_report.html")
-profile_test.to_file("test_report.html")
-profile_greek.to_file("geek_report.html")
+# path_report = './pandas_profiling_reports'
+# os.mkdir(path_report) if os.path.exists('./pandas_profiling_reports') else 0
+# profile_train.to_file("train_report.html")
+# profile_test.to_file("test_report.html")
+# profile_greek.to_file("geek_report.html")
 
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import pearsonr
+import pandas as pd
+import numpy as np
+
+def exhaustive_feature_engineering(df, target_col):
+    features = df.drop(columns=[target_col]).columns
+    results = []
+
+    for i in range(len(features)):
+        for j in range(i+1, len(features)):
+            # perform interaction and polynomial feature creation
+            interaction_feature = df[features[i]] * df[features[j]]
+            polynomial_feature = df[features[i]] ** 2
+            interaction_feature_name = f"{features[i]}_X_{features[j]}"
+            polynomial_feature_name = f"{features[i]}^2"
+
+            for new_feature, new_feature_name in zip([interaction_feature, polynomial_feature],
+                                                     [interaction_feature_name, polynomial_feature_name]):
+                # compute the mutual information with the target
+                mi = mutual_info_classif(new_feature.values.reshape(-1, 1), df[target_col])[0]
+
+                # compute the absolute Pearson correlation with the target
+                pearson_corr = abs(pearsonr(new_feature, df[target_col])[0])
+
+                # compute the feature importance
+                rf = RandomForestClassifier(n_estimators=10)
+                rf.fit(df[[features[i], features[j]]], df[target_col])
+                feat_imp = rf.feature_importances_
+
+                results.append([new_feature_name, mi, pearson_corr, feat_imp[0], feat_imp[1]])
+
+    # Create a DataFrame from results
+    df_results = pd.DataFrame(results, columns=['Feature', 'Mutual_Info', 'Pearson_Corr', 'Feat_Imp_1', 'Feat_Imp_2'])
+    return df_results
 
